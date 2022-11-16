b4037eef0ca08d7bd52b8507e4002b93e9260098
Genoveva Villablanca <gvillablancav.externo@bancofalabella.cl>
2022-11-16 19:47:17 -0300

    first commit

---
+++ src/westmarket/WestMarket.java
@@ -0,0 +1,14 @@
+package westmarket;
+
+import java.util.Scanner;
+
+public class WestMarket {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
