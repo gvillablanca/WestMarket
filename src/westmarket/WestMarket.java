a97e5364498b55a94d74e2acf8f6877f9a0fb584
Genoveva Villablanca <gvillablancav.externo@bancofalabella.cl>
2022-11-17 22:53:06 -0300

    fix: categorias

--- src/westmarket/WestMarket.java
+++ src/westmarket/WestMarket.java
@@ -46,9 +46,16 @@ public class WestMarket {
     }
     
     public static void registrarProducto(ArrayList<Producto> producto, ArrayList<Categoria> categoria){
-        Producto prod = new Producto();        
-        System.out.println("1");
-        menu(producto, categoria);
+        try{
+            Producto prod = new Producto();
+            System.out.println(categoria);
+            System.out.println("1");
+            menu(producto, categoria);
+        }
+        catch(Exception e){
+            System.out.println("error: " + e);
+            menu(producto, categoria);
+        }
     }
     
     public static void imprimirListado(ArrayList<Producto> producto, ArrayList<Categoria> categoria){
@@ -59,16 +66,15 @@ public class WestMarket {
                 for(int i = 0;i<producto.size();i++){
                         System.out.println("*********************************************************************");
                         System.out.println( "\n CODIGO: " +producto.get(i).getCodigo()+
-                                                                        "\n DESCRIPCION: " +producto.get(i).getDescripcion()+
-                                                                        "\n PRECIO: " +producto.get(i).getPrecio()+
-                                                                        "\n STOCK: " +producto.get(i).getStock()+
-                                                                        //"\n CATEGORIA: " +producto.get(i).getCategoria().getDescripcionCompleta());
-                                                                        "\n CATEGORIA: " );
+                                            "\n DESCRIPCION: " +producto.get(i).getDescripcion()+
+                                            "\n PRECIO: " +producto.get(i).getPrecio()+
+                                            "\n STOCK: " +producto.get(i).getStock()+
+                                            "\n CATEGORIA: " +producto.get(i).getCategoria());
                         System.out.println("*********************************************************************\n");
                 }
             }
             else{
-                  System.out.println("NO EXISTEN PRODUCTOS EN EL SISTEMA");
+                System.out.println("NO EXISTEN PRODUCTOS EN EL SISTEMA");
             }
         
         menu(producto, categoria);
@@ -80,22 +86,20 @@ public class WestMarket {
     }
     
     public static void imprimirListadoSimple(ArrayList<Producto> producto, ArrayList<Categoria> categoria){
-        System.out.println("2.5");
         try{
                 System.out.println("PRODUCTOS INGRESADOS");
                 if(producto.size()>0){
                     for(int i = 0;i<producto.size();i++){
                         System.out.println("*********************************************************************");
-                        System.out.println( "\n codigo: " +producto.get(i).getCodigo()+
-                                                                        "\n DESCRIPCION: " +producto.get(i).getDescripcion());
+                        System.out.println( "\n CODIGO: " +producto.get(i).getCodigo()+
+                                            "\n DESCRIPCION: " +producto.get(i).getDescripcion());
                         System.out.println("*********************************************************************\n");
-                 }
+                    }
                 }
                 else{
-                  System.out.println("NO EXISTEN PRODUCTOS EN EL SISTEMA"); 
+                    System.out.println("NO EXISTEN PRODUCTOS EN EL SISTEMA"); 
                 }
                 menu(producto, categoria);
-
         }
         catch(Exception e){
             System.out.println("ERROR EN SISTEMA, FAVOR VOLVER A REINTENTAR ");
@@ -111,38 +115,35 @@ public class WestMarket {
     }
     
     public static void salir(){
-        System.out.println("4");
+        try{
+            System.out.println("HASTA PRONTO");
+        }
+        catch(Exception e){
+            System.out.println("error: " + e); 
+        }
     }
     
     public static ArrayList<Categoria> categoria(ArrayList<Categoria> categoria){
-        Categoria cat = new Categoria();
-        int count = 0; 
-        
-        while(count <4){
-          if(count==0){
-            cat.setCodigo(1);
-            cat.setDescripcion("Bebidas");
-            cat.setDescripcionCompleta("Bebidas (cod: 1)");
-            categoria.add(cat);
-          }else if(count==1){
-            cat.setCodigo(2);
-            cat.setDescripcion("Congelados");
-            cat.setDescripcionCompleta("Congelados (cod: 2)");
-            categoria.add(cat);
-          }else if(count==2){
-            cat.setCodigo(3);
-            cat.setDescripcion("Lácteos");
-            cat.setDescripcionCompleta("Lácteos (cod: 3)");
-            categoria.add(cat);
-          }if(count==3){
-            cat.setCodigo(4);
-            cat.setDescripcion("Aseo");
-            cat.setDescripcionCompleta("Aseo (cod: 4)");
-            categoria.add(cat);            
-          }
-          count ++;
-        }
-        System.out.println(categoria);
+        Categoria cat1 = new Categoria();
+        Categoria cat2 = new Categoria();
+        Categoria cat3 = new Categoria();
+        Categoria cat4 = new Categoria();
+        cat1.setCodigo(1);
+        cat1.setDescripcion("Bebidas");
+        cat1.setDescripcionCompleta("Bebidas (cod: 1)");
+        categoria.add(cat1);
+        cat2.setCodigo(2);
+        cat2.setDescripcion("Congelados");
+        cat2.setDescripcionCompleta("Congelados (cod: 2)");
+        categoria.add(cat2);
+        cat3.setCodigo(3);
+        cat3.setDescripcion("Lácteos");
+        cat3.setDescripcionCompleta("Lácteos (cod: 3)");
+        categoria.add(cat3);
+        cat4.setCodigo(4);
+        cat4.setDescripcion("Aseo");
+        cat4.setDescripcionCompleta("Aseo (cod: 4)");
+        categoria.add(cat4);
         return categoria;
     }
 }
